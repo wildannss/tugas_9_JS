5c9a4311955c359518a215b03d22f2af352a197b
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:08:39 +0700

    tugas niomic
    tugas niomic

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function ulangin(){
+    let pegawai = {
+        nim: 12345,
+        nama: 'dani',
+        umur: 25,
+        hobi: 'membaca'
+    };
+    for(let w in pegawai){
+        console.log(pegawai[w]);
+    };
+};
+
+ulangin();
